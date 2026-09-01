a7b47ba26cc38b515499b8ca3047f2578b3ddb34
Alisa Koreneva <korenevaaliska@yandex.ru>
2026-09-01 17:37:30 +0300

    Diplom_3: Добавлены все Page Object класс)

--- src/main/java/pages/RegisterPage.java
+++ src/main/java/pages/RegisterPage.java
@@ -1,4 +1,76 @@
 package pages;
 
-public class RegisterPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class RegisterPage extends BasePage {
+
+    @FindBy(xpath = "//input[@name='name' and @type='text']")
+    private WebElement nameInput;
+
+    @FindBy(xpath = "(//input[@type='text'])[2]")
+    private WebElement emailInput;
+
+    @FindBy(xpath = "//input[@type='password']")
+    private WebElement passwordInput;
+
+    @FindBy(xpath = "//button[contains(text(), 'Зарегистрироваться')]")
+    private WebElement registerButton;
+
+    @FindBy(xpath = "//a[contains(text(), 'Войти')]")
+    private WebElement loginLink;
+
+    @FindBy(xpath = "//p[contains(text(), 'Некорректный пароль')]")
+    private WebElement errorMessage;
+
+    public RegisterPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterName(String name) {
+        wait.until(ExpectedConditions.visibilityOf(nameInput)).sendKeys(name);
+    }
+
+    public void enterEmail(String email) {
+        wait.until(ExpectedConditions.visibilityOf(emailInput)).sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        wait.until(ExpectedConditions.visibilityOf(passwordInput)).sendKeys(password);
+    }
+
+    public void clickRegisterButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(registerButton)).click();
+    }
+
+    public void register(String name, String email, String password) {
+        enterName(name);
+        enterEmail(email);
+        enterPassword(password);
+        clickRegisterButton();
+    }
+
+    public void clickLoginLink() {
+        wait.until(ExpectedConditions.elementToBeClickable(loginLink)).click();
+    }
+
+    public boolean isErrorMessageVisible() {
+        try {
+            return wait.until(ExpectedConditions.visibilityOf(errorMessage)).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getErrorMessageText() {
+        try {
+            return wait.until(ExpectedConditions.visibilityOf(errorMessage)).getText();
+        } catch (Exception e) {
+            return "";
+        }
+    }
 }
